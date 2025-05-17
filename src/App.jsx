d40f2f8a534b5a20c576dfc5b21ade2cce4fbb52
sahil <sahilattar87860@gmail.com>
2025-05-17 12:41:07 +0530

    Config : Project Config

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+    <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+  </div>
+  )
+}
+
+export default App
